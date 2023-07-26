4b8f0509837941d028630844e63861026c738918
Russell Hofvendahl <rhofvendahl@gmail.com>
2023-07-26 00:16:08 -0700

    feat: update reduce filesize script to remove lines

--- tr_scripts/reduce_filesize.py
+++ tr_scripts/reduce_filesize.py
@@ -30,7 +30,6 @@ def decimate_obj(input_dir: str, output_dir: str, name: str, decimate_ratio: flo
         if node.type == "TEX_IMAGE":
             tex_node: bpy.types.ShaderNodeTexImage = node
             tex_filepath = tex_node.image.filepath
-            print("FILEPATH", tex_filepath)
             if os.path.isfile(tex_filepath):
                 # tex_filename = os.path.basename(tex_filepath)
                 shutil.copy(
@@ -47,9 +46,23 @@ def decimate_obj(input_dir: str, output_dir: str, name: str, decimate_ratio: flo
         file.write(content)
 
 
+# Decimate leaves a few lines between verticies, which apparently mess up obj files for some purposes. It's likely possible
+# to not create those lines, but easier IMO to just edit the obj.
+def remove_lines(filepath):
+    with open(filepath, "r") as f:
+        lines = f.readlines()
+
+    with open(filepath, "w") as f:
+        for line in lines:
+            if not line.startswith("l "):
+                f.write(line)
+
+
 def reduce_tex_size(filepath: str, new_size: int):
     pass
 
+
+# TODO: Refactor below (DRY it up)
 
 input_base = os.path.abspath("../data/models/obj/reduced")
 output_base = os.path.abspath("../data/models/obj/reduced_decp2_256")
@@ -61,6 +74,8 @@ for name in MODEL_NAMES[:-1]:
 
     decimate_obj(input_dir, output_dir, name, 0.2)
 
+    remove_lines(os.path.join(output_dir, name + ".obj"))
+
     reduce_tex_size(os.path.join(output_dir, "texgen_2.png"), 256)
 
 output_base = os.path.abspath("../data/models/obj/reduced_decp5_512")
@@ -71,5 +86,7 @@ for name in MODEL_NAMES[-1:]:
     os.makedirs(output_dir, exist_ok=True)
 
     decimate_obj(input_dir, output_dir, name, 0.5)
+
+    remove_lines(os.path.join(output_dir, name + ".obj"))
 
     reduce_tex_size(os.path.join(output_dir, "texgen_2.png"), 512)
