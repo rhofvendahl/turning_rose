5fab22792c40a72fb664b3d24c0b4daf90665d65
Russell Hofvendahl <rhofvendahl@gmail.com>
2023-07-26 21:53:00 -0700

    feat: filter points correctly for red

--- tr_scripts/align_models.py
+++ tr_scripts/align_models.py
@@ -1,8 +1,12 @@
 import os
 
+import colorsys
+
 import numpy as np
 import open3d as o3d
 from PIL import Image
+
+from matplotlib import colors
 
 MODEL_NAMES = [
     "2023-07-19_19",
@@ -45,7 +49,7 @@ def get_point_cloud(dirpath: str, name: str):
     img_data = np.array(img)
     colors = []
     black = np.array([0.0, 0.0, 0.0])
-    for i in range(len(vertices)):
+    for i, _ in enumerate(vertices):
         uv = vertex_uvs[i]
         if uv is None:
             colors.append(black)
@@ -62,13 +66,62 @@ def get_point_cloud(dirpath: str, name: str):
     return point_cloud
 
 
+def check_hue(hue, target, tolerance):
+    # Hue wraps around, i.e. 0 and 360 are the same shade of red
+    bound_1 = (target - tolerance) % 1.0
+    bound_2 = (target + tolerance) % 1.0
+    # This makes it a bit tricky to tell whether a hue is within these bounds, as one bound may have wrapped around
+    if bound_1 <= bound_2:
+        within_bounds = bound_1 <= hue <= bound_2
+    else:
+        within_bounds = hue >= bound_1 or hue <= bound_2
+    return within_bounds
+
+
+def filter_by_hsv(pcd, target, tolerances):
+    # Convert point cloud colors to np array
+    rgb_colors = np.asarray(pcd.colors)
+
+    # Convert RGB to HSV
+    hsv_colors = colors.rgb_to_hsv(rgb_colors)
+
+    # Filter the points
+    filtered_points = []
+    filtered_colors = []
+    for i, hsv_color in enumerate(hsv_colors):
+        hue_match = check_hue(hsv_color[0], target[0], tolerances[0])
+        # Saturation is straightforward
+        saturation_match = (
+            target[1] - tolerances[1] <= hsv_color[1] <= target[1] + tolerances[1]
+        )
+        # Value is straightforward
+        value_match = (
+            target[2] - tolerances[2] <= hsv_color[2] <= target[2] + tolerances[2]
+        )
+
+        if hue_match and saturation_match and value_match:
+            filtered_points.append(pcd.points[i])
+            filtered_colors.append(pcd.colors[i])
+
+    # Create a new point cloud with the filtered points
+    filtered_pc = o3d.geometry.PointCloud()
+    filtered_pc.points = o3d.utility.Vector3dVector(filtered_points)
+    filtered_pc.colors = o3d.utility.Vector3dVector(filtered_colors)
+
+    return filtered_pc
+
+
 def main():
     name = MODEL_NAMES[-1]
     dirpath = os.path.abspath(os.path.join("../data/models/obj/reduced_same", name))
 
-    cloud = get_point_cloud(dirpath, MODEL_NAMES[-1])
-    print("CLOUD", type(cloud), cloud)
-    o3d.visualization.draw_geometries([cloud])
+    pcd = get_point_cloud(dirpath, name)
+
+    # Works well for final model
+    target = [358 / 360.0, 80 / 100.0, 60 / 100.0]
+    tolerances = [3 / 360.0, 20 / 100.0, 40 / 100.0]
+    pcd_filtered = filter_by_hsv(pcd, target, tolerances)
+    o3d.visualization.draw_geometries([pcd_filtered])
 
 
 if __name__ == "__main__":
