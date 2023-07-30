564441493ba5f07239c1b8256258f2b9d1a0feda
Russell Hofvendahl <rhofvendahl@gmail.com>
2023-07-29 23:55:49 -0700

    feat: update controls

--- tr-web/src/components/Controls.tsx
+++ tr-web/src/components/Controls.tsx
@@ -5,34 +5,38 @@ import { Frame } from '../hooks/useFrame';
 const Controls = ({ frames, currentFrame, setCurrentFrame }: {
   frames: Frame[],
   currentFrame: Frame | null,
-  setCurrentFrame: (x: any) => void}
+  setCurrentFrame: (x: any) => void
+}
 ) => {
+  // 'play' is whether the viewer should be playing
   const [play, setPlay] = useState(false);
+  // 'playInterval' keeps track of whether viewer actually is playing
   const [playInterval, setPlayInterval] = useState<number | null>(null);
-  
-  // Give access to mutable currentFrame within playInterval below
-  const currentFrameRef = useRef(currentFrame);
-  useEffect(() => {
-    currentFrameRef.current = currentFrame;
-  }, [currentFrame]);
+
+  const playIndex = useRef(0);
 
   useEffect(() => {
     if (currentFrame === null) {
       return;
     }
     if (play) {
+      // Only set an interval if none exists
       if (playInterval === null) {
+        // Used within the interval to keep track of play
+        playIndex.current = 0;
         const interval = window.setInterval(() => {
-          const current = currentFrameRef.current;
-          if (current === null) {
-            return;
+          if (playIndex.current < frames.length) {
+            setCurrentFrame(frames[playIndex.current]);
+          } else {
+            // Stop at the end
+            setPlay(false);
           }
-          const nextFrame = frames[(current.index + 1) % frames.length];
-          setCurrentFrame(nextFrame);
-        }, 500);
+          playIndex.current += 1;
+        }, 333);
         setPlayInterval(interval);
       }
     } else {
+      // Only clear an interval if one exists
       if (playInterval !== null) {
         window.clearInterval(playInterval);
         setPlayInterval(null);
@@ -42,17 +46,26 @@ const Controls = ({ frames, currentFrame, setCurrentFrame }: {
 
   return (
     <div>
-        {frames.map((_, index) => (
-          <button key={index} onClick={() => setCurrentFrame(frames[index])}>
-            Frame {index + 1}
-          </button>
-        ))}
-        <button onClick={() => setPlay(true)}>Play</button>
-        <button onClick={() => setPlay(false)}>Pause</button>
-      <div id="frames">
-      </div>
-      <div id="play-pause">
-      </div>
+      {/* {frames.map((_, index) => (
+        <button key={index} onClick={() => setCurrentFrame(frames[index])}>
+          {frames[index].path.split("/").pop()}
+        </button>
+      ))} */}
+      <button onClick={() => {
+        if (currentFrame !== null && currentFrame.index > 0) {
+          const nextFrame = frames[currentFrame.index - 1];
+          setCurrentFrame(nextFrame);
+        }
+      }}>Prev</button>
+      <button onClick={() => setPlay(true)}>Play</button>
+      <button onClick={() => setPlay(false)}>Pause</button>
+      <button onClick={() => {
+        setPlay(false);
+        if (currentFrame !== null && currentFrame.index + 1 < frames.length) {
+          const nextFrame = frames[currentFrame.index + 1];
+          setCurrentFrame(nextFrame);
+        }
+      }}>Next</button>
     </div>
   );
 }
