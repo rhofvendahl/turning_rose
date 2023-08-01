1812343fe60c6f76161a5bf2a557e7be68e041c0
Russell Hofvendahl <rhofvendahl@gmail.com>
2023-08-01 15:45:47 -0700

    feat: add utils for manipulating photogram dirs

--- tr_scripts/utils.py
+++ tr_scripts/utils.py
@@ -1,26 +1,73 @@
 import os
 import shutil
 
-from constants import NAMES
+import pandas as pd
+
+from constants import (
+    RAW_OUTPUTS_DIRPATH,
+    IMAGE_DATA_FILEPATH,
+    RAW_IMAGES_DIRPATH,
+    TEMP_OUTPUTS_DIRPATH,
+)
+
+# TODO: Only return names of dirs that actually have content
 
 
-# Returns whether hue is within bounds
-def check_hue(hue, target, tolerance):
-    # NOTE: I don't like this, TODO: change it later
-    if target is None:
-        return True
+def get_names(dirpath: str = RAW_OUTPUTS_DIRPATH):
+    names = []
+    for _, dirs, _ in os.walk(dirpath):
+        for d in dirs:
+            # Model names should be along the lines of '0_2023-07-31_19'
+            if len(d.split("_")) == 3:
+                names.append(d)
 
-    # Hue wraps around, i.e. 0 and 360 are the same shade of red
-    bound_1 = (target - tolerance) % 1.0
-    bound_2 = (target + tolerance) % 1.0
-    # This makes it a bit tricky to tell whether a hue is within these bounds, as one bound may have wrapped around
-    if bound_1 <= bound_2:
-        within_bounds = bound_1 <= hue <= bound_2
-    else:
-        within_bounds = hue >= bound_1 or hue <= bound_2
-    if within_bounds:
-        print("WITHIN BOUNDS")
-    return within_bounds
+    return names
+
+
+def get_capture_names():
+    images_df = pd.read_csv(IMAGE_DATA_FILEPATH)
+    return images_df["capture_name"].unique().tolist()
+
+
+def get_images_from_capture(capture_name: str):
+    images_df = pd.read_csv(IMAGE_DATA_FILEPATH)
+    matches = images_df.loc[images_df["capture_name"] == capture_name]
+    return matches["name"].tolist()
+
+
+def remove_empty_names():
+    names = get_names()
+    for name in names:
+        dirpath = os.path.join(RAW_OUTPUTS_DIRPATH, name)
+        if len(os.listdir(dirpath)) == 0:
+            try:
+                os.rmdir(dirpath)
+                print(f"Removed empty: {name}")
+            except:
+                print(f"Failed to remove empty: {name}")
+
+
+# Yikes! Dangerous territory
+def wipe_temp():
+    # Hardcoding this here to make absolutely sure it doesn't get passed something weird
+    temp_dirpath = "/Volumes/T7/turning_rose/outputs/temp"
+    if os.path.exists(temp_dirpath):
+        shutil.rmtree(temp_dirpath)
+        os.makedirs(temp_dirpath)
+
+
+def copy_to_temp(capture_name):
+    wipe_temp()
+
+    image_names = get_images_from_capture(capture_name)
+    for image_name in image_names:
+        input_filepath = os.path.join(RAW_IMAGES_DIRPATH, image_name)
+        output_filepath = os.path.join(TEMP_OUTPUTS_DIRPATH, image_name)
+
+        if os.path.exists(input_filepath):
+            shutil.copy2(input_filepath, output_filepath)
+        else:
+            print("MISSING IMAGE", image_name)
 
 
 def copy_assets(
@@ -32,7 +79,7 @@ def copy_assets(
     use_jpg=False,
 ):
     if names == None:
-        names = NAMES
+        names = get_names()
     img_ext = ".jpg" if use_jpg else ".png"
 
     for name in names:
