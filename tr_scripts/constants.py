e4b3ff19c3afd53ef189666870a0e5f8318356ec
Russell Hofvendahl <rhofvendahl@gmail.com>
2023-08-01 15:43:50 -0700

    feat: update path constants to ref external hdd

--- tr_scripts/constants.py
+++ tr_scripts/constants.py
@@ -1,25 +1,14 @@
-NAMES = [
-    "2023-07-18_07",
-    "2023-07-18_14",
-    "2023-07-18_19",
-    "2023-07-19_01",
-    "2023-07-19_07",
-    "2023-07-19_13",
-    "2023-07-19_19",
-    "2023-07-20_01",
-    "2023-07-20_07",
-    "2023-07-20_13_final",
-]
-
-BLENDER_EXPORT_BASE = "../data/models/obj/blender_export"
-
-# TODO:Probably time for a new approach
-ALIGNED_BASE = "../data/models/obj/intermediate"
-CLEANED_BASE = "../data/models/obj/intermediate"
-DECIMATED_BASE = "../data/models/obj/intermediate"
-
-GLTF_DIR = "../tr-web/public/db/gltf/auto"
-INDEX_FILEPATH = "../tr-web/src/assets/json/modelPaths.json"
+RAW_IMAGES_DIRPATH = (
+    "/Volumes/T7/turning_rose/raw_images/2023_06_15_evening_to_2023_07_20_noon"
+)
+RAW_OUTPUTS_DIRPATH = "/Volumes/T7/turning_rose/outputs/raw"
+TEXTURE_OUTPUTS_DIRPATH = "/Volumes/T7/turning_rose/outputs/textures"
+INTERMEDIATE_OUTPUTS_DIRPATH = "/Volumes/T7/turning_rose/outputs/intermediate"
+COMPLETE_OUTPUTS_DIRPATH = "/Volumes/T7/turning_rose/outputs/complete"
+METADATA_OUTPUTS_DIRPATH = "/Volumes/T7/turning_rose/outputs/metadata"
+# Also hardcoded in utils.py, to be absolutely sure the wrong dir doesn't get wiped
+TEMP_OUTPUTS_DIRPATH = "/Volumes/T7/turning_rose/outputs/temp"
+IMAGE_DATA_FILEPATH = "/Volumes/T7/turning_rose/outputs/metadata/image_data.csv"
 
 # Useful:
 # python align_models.py && python clean_models.py && python decimate_models.py && python export_gltf.py
