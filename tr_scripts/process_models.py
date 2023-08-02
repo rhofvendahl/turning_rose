663e154095a9a90981547353c24ff927a1f990e6
Russell Hofvendahl <rhofvendahl@gmail.com>
2023-08-01 22:27:59 -0700

    fix: wrong order of commands

--- tr_scripts/process_models.py
+++ tr_scripts/process_models.py
@@ -19,16 +19,14 @@ def perform_blender_operations(
     bpy.ops.import_scene.obj(filepath=os.path.join(source_dirpath, filename))
     obj = bpy.context.selected_objects[0]
 
-    # This wasn't in the script for decimation but was in the remove outer script;
-    # seems unlikely to mess things up though.
-    bpy.ops.object.mode_set(mode="EDIT")
-
     if decimate_ratio != None:
         modifier: bpy.types.DecimateModifier = obj.modifiers.new("decimate", "DECIMATE")
         modifier.ratio = decimate_ratio
         bpy.ops.object.modifier_apply(modifier=modifier.name)
 
     if radius != None:
+        bpy.ops.object.mode_set(mode="EDIT")
+
         bm = bmesh.from_edit_mesh(obj.data)
         verts_to_remove = [
             v for v in bm.verts if math.sqrt(v.co.x**2 + v.co.z**2) > radius
@@ -36,7 +34,8 @@ def perform_blender_operations(
         bmesh.ops.delete(bm, geom=verts_to_remove, context="VERTS")
         bmesh.update_edit_mesh(obj.data)
 
-    bpy.ops.object.mode_set(mode="OBJECT")
+        bpy.ops.object.mode_set(mode="OBJECT")
+
     bpy.ops.export_scene.obj(
         filepath=os.path.join(dest_dirpath, filename), use_selection=True
     )
@@ -53,7 +52,7 @@ def process_models(
     decimate_ratio = decimate_ratio if decimate_ratio != None and decimate else None
     radius = 0.5 if remove_outer else None
 
-    print("DECIMATING")
+    print("PROCESSING MODELS")
     for i, name in enumerate(names):
         print(i, name)
         source_dirpath = os.path.join(source_base_dirpath, name)
